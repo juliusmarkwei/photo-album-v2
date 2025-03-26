2545536512f6bd8d117af45dae240e61b821ac18
Julius Markwei <julius.markwei@amalitech.com>
2025-03-26 16:07:03 +0000

    Resolve merge conflict in data-source.ts

--- app/utils/db/data-source.ts
+++ app/utils/db/data-source.ts
@@ -1,17 +1,25 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Photo } from "./entity/Photo";
-import { getDbCredentials } from "../parameterStoreConfig";
+// import { getDbCredentials } from "../parameterStoreConfig";
 
 export const AppDataSource = async () => {
-    const { username, password, host, dbName, port } = await getDbCredentials();
+    // const { username, password, host, dbName, port } = await getDbCredentials();
     return new DataSource({
         type: "postgres",
+<<<<<<< Updated upstream
         host,
         port: Number(port),
         username,
         password,
         database: dbName,
+=======
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        password: "postgres",
+        database: "photoalbum",
+>>>>>>> Stashed changes
         synchronize: true,
         logging: false,
         entities: [Photo],
